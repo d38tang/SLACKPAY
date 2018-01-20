04a13d69f0c38adc29463f4204691fcaba0d4b2a
David Tang <tangdavid9001@gmail.com>
2018-01-20 18:11:32 -0500

    slack pay backend api

--- index.js
+++ index.js
@@ -8,43 +8,61 @@ app.use(bodyParser.json())
 
 // Helper functions ----------------------------------------------------------
 
-function addEmployee(employee, db, res) {
-  db.collection("employees", function(error, collection){
+function addUser(user, db, res) {
+  db.collection("users", function(error, collection){
     if (error) throw new Error(error);
-    collection.insert(employee, function(error, employee){
+    collection.insert(user, function(error, user){
       if (error) throw new Error(error);
-      res.send(JSON.stringify(employee))
+      res.send(JSON.stringify(user))
     })
   })
 }
 
-function deleteEmployee(employee, db, res) {
-  db.collection("employees", function(error, collection){
-    if (error) throw new Error(error);
-    collection.remove({_id: new MongoObjectID(employee._id)}, function(error, result){
-      if (error) throw new Error(result);
-      res.send(result)
+function addTransaction(senderId, receiverId, amount, date, db, res) {
+  let time = {
+    year: date.getFullYear(),
+    date: date.getDate(),
+    month: date.getMonth() + 1,
+    hour: date.getHours(),
+    minutes: date.getMinutes()
+  }
+
+  let transaction = {
+    senderId: senderId,
+    receiverId: receiverId,
+    amount: amount,
+    time: time
+  }
+
+  db.collection("users", function(error, collection) {
+    collection.findOne({id: senderId}, function(error, sender) {
+      if (error) throw new Error(error);
+      collection.findOne({id: receiverId}, function(error, receiver) {
+        if (error) throw new Error(error);
+        transaction.senderName = sender.name
+        transaction.receiverName = receiver.name
+        let newSenderTransaction = sender.transactions
+        newSenderTransaction.push(transaction)
+        let newReceiverTransaction = receiver.transactions
+        newReceiverTransaction.push(transaction)
+        collection.update({id: senderId}, {$set: {transactions: newSenderTransaction}}, function (error, user){
+          if (error) throw new Error(error);
+          collection.update({id: receiverId}, {$set: {transactions: newReceiverTransaction}}, function (error, user){
+            if (error) throw new Error(error);
+            res.send("Transaction Added")
+          })
+        })
+      })
     })
   })
 }
 
-function getEmployeeDetails(db, res) {
-  db.collection("employees", function(error, collection){
+function getUserTransactionHistory(userId, db, res) {
+  db.collection("users", function(error, collection){
     if (error) throw new Error(error);
-    collection.find().toArray(function(err, employees){
+    collection.findOne({id: userId}, function(error, user) {
       if (error) throw new Error(error);
-      res.send(JSON.stringify(employees))
-    })
-  })
-}
-
-function updateEmployeeDetails(employee, db, res) {
-  db.collection("employees", function(error, collection){
-    if (error) throw new Error(error);
-    console.log(employee._id)
-    collection.update({_id: new MongoObjectID(employee._id)}, employee, function(err, employee){
-      if (error) throw new Error(error);
-      res.send(JSON.stringify(employee))
+      res.send(JSON.stringify(user.transactions))
     })
   })
 }
@@ -59,23 +77,21 @@ MongoClient.connect(config.databaseUrl, function(error, database){
   if (error) throw new Error(error);
   db = database.db("slackpay");
 
-  app.get('/', function(req, res) {
-    getEmployeeDetails(db, res)
+  app.post('/adduser', function(req, res) {
+    let user = req.body.body
+    addUser(user, db, res)
   })
 
-  app.post('/add', function(req, res) {
-    let employee = req.body.body
-    addEmployee(employee, db, res)
+  app.post('/addtransaction', function(req, res) {
+    let senderId = req.body.body.senderId
+    let receiverId = req.body.body.receiverId
+    let amount = req.body.body.amount
+    addTransaction(senderId, receiverId, amount, new Date(), db, res)
   })
 
-  app.post('/delete', function(req, res) {
-    let employee = req.body.body
-    deleteEmployee(employee, db, res)
-  })
-
-  app.post('/change', function(req, res){
-    let employee = req.body.body
-    updateEmployeeDetails(employee, db, res)
+  app.get('/history', function(req, res) {
+    let userId = req.query.id
+    getUserTransactionHistory(userId, db, res)
   })
 
   app.listen(app.get('port'), function() {
